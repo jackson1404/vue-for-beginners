9c40a80e309eefdb9a827b9f4644b491b6d91192
jackson1404 <jackson14042004@gmail.com>
2025-07-23 22:52:42 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>My Firt Vue</h1>
+    <p>Likes :  {{ likes }}</p>
+    <p v-if="likes > 0">Like > 0 </p>
+    <p v-else>Like = 0</p>
+    <button v-on:click="LikeAndSubscribe()">Like And Subscribe</button>
+    <input v-model="likes"></input>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes : 0
+    }
+  },
+  methods : {
+    LikeAndSubscribe(){
+      this.likes++;
+      alert('like increase')
+    }
+  }
+}
+</script>
