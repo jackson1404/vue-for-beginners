0a9aba18ffab5b45d8e322c1ced43bf0ae74828e
jackson1404 <jackson14042004@gmail.com>
2025-07-23 23:26:46 +0630

    modify testing

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
     <p>Likes :  {{ likes }}</p>
     <p v-if="likes > 0">Like > 0 </p>
     <p v-else>Like = 0</p>
-    <button v-on:click="LikeAndSubscribe()">Like And Subscribe</button>
+    <button @click="LikeAndSubscribe()">Like And Subscribe</button>
+    <button @click="likeStatus = !likeStatus">Like Button</button>
+    <p v-if="likeStatus">Like Clicked</p>
+    <p v-else>Like not clicked</p>
     <input v-model="likes"></input>
+    <p>Input : {{ likes }}</p>
+    <p v-show="unLikes > 0">UnLikes {{ unLikes }}</p>
   </div>
   
 </template>
@@ -14,7 +19,9 @@
 export default {
   data() {
     return {
-      likes : 0
+      likes : 0,
+      unLikes : 0,
+      likeStatus : false
     }
   },
   methods : {
