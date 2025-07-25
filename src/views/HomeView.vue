10535b8c167cfdd247da2b6b875e91f04caad1af
jackson1404 <jackson14042004@gmail.com>
2025-07-25 23:42:32 +0630

    modify testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,27 @@
 <template>
     <h1> Home Page </h1>
     <p> Welcome to home page</p>
+
+    <form @submit.prevent="submitTask">
+        <h1>Task form</h1>
+        <input type="text" v-model="newTask" id="newTask" name="newTask"></input>
+        <button type="submit">Submit</button>"
+    </form>
+
+    <ul>
+        <li v-for="(task, index) in tasks" :key="task">{{ task }}</li>
+    </ul>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tasks = ref(['Task1', 'Task2']);
+const newTask = ref('');
+const submitTask = () =>{
+    if(newTask.value.trim() !== ''){
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+    }
+};
+</script>
