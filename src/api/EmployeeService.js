5b374a8b37e2fa65b47c4595bd80fffb0ab8f03e
jackson1404 <jackson14042004@gmail.com>
2025-07-27 23:08:19 +0630

    modify employee service

--- src/api/EmployeeService.js
+++ src/api/EmployeeService.js
@@ -1,11 +1,13 @@
-const BASE_URL = 'http://localhost:8080/api/v1/users';
+import axios from 'axios';
 
-export async function fetchUsers() {
-    const apiData = await fetch (BASE_URL + '/getAllUsers');
-    if(!apiData.ok){ throw new Error('Fail to load users')};
-    return await apiData.json();
-}
+const BASE_URL = 'http://localhost:8080/api/v1';
 
-export async function addNewUser() {
-    
-}
+export const fetchUsers = () => axios.get(`${BASE_URL}/getAllUsers`).then(res => res.data);
+
+export const registerUser = (userData) => axios.post(`${BASE_URL}/auth/userSignUp`, userData).then(res => res.data);
+
+export const getUserById = (userId) => axios.get(`${BASE_URL}/getUserById`, {params: {userId}}).then(res => res.data);
+
+export const updateUser = (userId, userData) => axios.put(`${BASE_URL}/updateUser`, {params: {userId}}, userData).then(res => res.data);
+
+export const deleteUser = (userId) => axios.delete(`${BASE_URL}/deleteUser`, {params: {userId}}).then(res => res.data);
--- src/views/AddNewUserView.vue
+++ src/views/AddNewUserView.vue
@@ -1,11 +1,24 @@
 <template>
     <h1>Add new User</h1>
     <form @submit.prevent="submitForm">
-        <input type="text" v-model="newUser.userName"></input>
+        <p>User Name: </p>
+        <input type="text" v-model="newUser.userName" placeholder="Enter User Name"></input>
+        <br></br>
+        <p>User Email: </p>
+        <input type="email" v-model="newUser.userEmail" placeholder="Enter User Email"></input>
+        <br></br>
+        <p>User Password: </p>
+        <input type="password" v-model="newUser.userPassword" placeholder="Enter User Password"></input>
+        <br></br>
+        <p>User Address: </p>
+        <input type="text" v-model="newUser.userAddress" placeholder="Enter User Address"></input>
+        <br></br>
+        <button type="submit">Sign Up</button>
     </form>
 </template>
 <script setup>
 import { ref } from 'vue';
+import {registerUser} from '@/api/EmployeeService.js';
 
 const newUser = ref({
     userName: '',
@@ -14,4 +27,13 @@ const newUser = ref({
     userAddress: '',
     isEmailVerificationRequired: false
 })
+const submitForm = async () => {
+    try{
+        const response = await registerUser(newUser.value);
+        alert('User successfully registered');
+    }catch(error){
+        alert('Error submit new user', error);
+    }
+
+}
 </script>   
