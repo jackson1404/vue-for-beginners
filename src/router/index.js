b7ff7d3dd7627706596ab56b0a01435849534a01
jackson1404 <jackson14042004@gmail.com>
2025-08-05 22:49:12 +0630

    modify router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter , createWebHistory} from "vue-router";
-
+import { useAuthStore } from "../stores/auth";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import UserListView from "@/views/UserListView.vue";
 import AddNewUserView from '@/views/AddNewUserView.vue';
 import EditUserView from '@/views/EditUserView.vue';
+import { use } from "react";
 
 const routes = [
     {
@@ -38,5 +39,15 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    const auth = useAuthStore();
+    if (to.path !== '/login' && !auth.token){
+        next('/login')
+    } else {
+        next()
+    }
+})
+
 
 export default router;
